a8756a717c85b1b297257244e1090ef8581bab9d
Fatemeh-S17 <fatemeh17seyedmomeni@gmail.com>
2023-03-15 17:11:58 +0330

    First program

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+    $books = [
+        [
+            'title' => 'The first book',
+            'Author' => 'Author1',
+            'publish_date' => '1395',
+            'price' => 100000
+        ],
+        [
+            'title' => 'The new first book',
+            'Author' => 'Author1',
+            'publish_date' => '1395',
+            'price' => 120000
+        ],
+        [
+            'title' => 'The second book',
+            'Author' => 'The second Author',
+            'publish_date' => '1399',
+            'price' => 150000
+        ],
+        [
+            'title' => 'The third book',
+            'Author' => 'The third Author',
+            'publish_date' => '1395',
+            'price' => 150000
+        ]
+    ];
+    function filter($books,$fn)
+    {
+        $filteredbook = [];
+        foreach($books as $book)
+        {
+            if($fn($book))
+            {
+                $filteredbook[] = $book;
+            }
+        }
+        return $filteredbook;
+    }
+    $filteredbook = filter($books,function($book){
+        if($book['price'] <=150000)
+        {
+            return true;
+        }
+        return false;
+
+    });
+    require("index.view.php");
+?>
