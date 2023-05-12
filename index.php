b55ba6897a460ddafe6f36032f4f01abced7ace4
Fatemeh-S17 <fatemeh17seyedmomeni@gmail.com>
2023-05-12 13:51:30 +0430

    Database continuation

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-
+$dbname="webprogramming"
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password,$dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
+//exit("Connected successfully");
     $books = [
         [
             'title' => 'The first book',
